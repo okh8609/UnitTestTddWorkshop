e80bce3df17fdc08c72e80407e9d8eeadc08d263
student <student@204-04>
2021-03-15 15:38:00 +0800

    Add project files.

---
+++ OnlineStoreBackend.Test.Unit/RegistrationManagerTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStoreBackend.Test.Unit
+{
+    [TestFixture]
+    public class RegistrationManagerTest
+    {
+        [Test]
+        public void AcceptsAccountWhenValidUsernamePasswordAreRegistered() // 動詞為首：當合法的username、password輸入時，會接受
+        {
+            // Arrange
+            // 安排測試的先決條件
+            RegistrationManager regMgr = new RegistrationManager();
+
+            // Act
+            // 要觸發的動作
+            bool result = regMgr.RegisterAccount("usr", "NtuCsie999");
+
+            // Assert
+            // 預期會由什麼樣的結果
+            Assert.True(result);
+        }
+    }
+}
---
+++ OnlineStoreBackend/RegistrationManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OnlineStoreBackend
+{
+    public class RegistrationManager
+    {
+        public bool RegisterAccount(string v1, string v2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
